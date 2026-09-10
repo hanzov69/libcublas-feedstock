7b3491ff6b65e3a896a95bfd2b300aa345f8b770
Christian Sullivan <chrisullivan@nvidia.com>
2026-09-10 12:45:02 -0400

    Update for CUDA 13.4.1

--- recipe/compile_sample_code.bat
+++ recipe/compile_sample_code.bat
@@ -1,3 +1,9 @@
+if "%TARGET_PLATFORM%" == "win-arm64" (
+    set CUDA_ARCH=arm64
+) else (
+    set CUDA_ARCH=x64
+)
+
 @echo on
 
-cl test.c /I%LIBRARY_INC% /I%LIBRARY_INC%\targets\x64 /link /LIBPATH:%LIBRARY_LIB%\x64 cublas.lib
+cl test.c /I%LIBRARY_INC% /I%LIBRARY_INC%\targets\%CUDA_ARCH% /link /LIBPATH:%LIBRARY_LIB%\%CUDA_ARCH% cublas.lib
